d4597a770973569e8fdc0de672820b21960568d1
Dmitry <sheklein.dima@gmail.com>
2018-09-03 11:58:36 +0300

    Появление окна без метки

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ let comments = [];
 let modal = document.getElementById('modal');
 let modalCloseBtn = document.querySelector('.modal__close')
 let modalSaveReview = modal.querySelector('.btn.btn--save')
+let lastAddress;
+let lastCoords;
 
 const init = () => {
     const map = new ymaps.Map('map', {
@@ -15,62 +17,19 @@ const init = () => {
         searchControlProvider: 'yandex#search'
     });
 
-    const clusterer = new ymaps.Clusterer({
-        /**
-         * Через кластеризатор можно указать только стили кластеров,
-         * стили для меток нужно назначать каждой метке отдельно.
-         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/option.presetStorage.xml
-         */
-        preset: 'islands#invertedVioletClusterIcons',
-        /**
-         * Ставим true, если хотим кластеризовать только точки с одинаковыми координатами.
-         */
-        groupByCoordinates: false,
-        /**
-         * Опции кластеров указываем в кластеризаторе с префиксом "cluster".
-         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ClusterPlacemark.xml
-         */
-        clusterDisableClickZoom: true,
-        clusterHideIconOnBalloonOpen: false,
-        geoObjectHideIconOnBalloonOpen: false
-    })
-    let geoObjects = [];
-
     map.events.add('click', e => {
-        let coords = e.get('coords');
+        lastCoords = e.get('coords');
         let position = e.get('position');
-        let coordToAdress = ymaps.geocode(coords);
+        let coordToAdress = ymaps.geocode(lastCoords);
         let id = Date.now();
 
-        coordToAdress.then(res=> {
+        coordToAdress.then(res => {
             let obj = res.geoObjects.get(0);
-            let lastAddress = obj.properties.get('name');
-           
-            // Создаём макет содержимого.
-            let MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-                '<i class="map-pin active fa fa-map-marker" aria-hidden="true"></i>'
-            )            
-            const placemark = new ymaps.Placemark(coords, {}, {
-                // Необходимо указать данный тип макета.
-                iconImageHref: '',
-                // Размеры метки.
-                iconImageSize: [29, 50],
-                // Смещение левого верхнего угла иконки относительно
-                // её "ножки" (точки привязки).
-                iconImageOffset: [-14, -25],                
-                iconLayout: 'default#imageWithContent',
-                iconContentLayout: MyIconContentLayout
-            })
 
-            placemark.properties.set('test-id', id);
-            placemark.properties.set('type', 'geoMarker');
-            placemark.properties.set('address', lastAddress);
-            
-            map.geoObjects.add(placemark);
-            // geoObjects.push(placemark)
-
-            addToComments(id, lastAddress)
+            lastAddress = obj.properties.get('name');
+      
             showModal(id, position, lastAddress)
+            
         })
     })
 
@@ -80,19 +39,98 @@ const init = () => {
         let id = marker.properties.get('test-id');
         let position = e.get('position');
         let lastAddress = marker.properties.get('address');
- 
+
         if (type === 'geoMarker') {
-            showModal( id, position, lastAddress)
-        }            
+            showModal(id, position, lastAddress)
+        }
     })
+
+    modalSaveReview.addEventListener('click', e => {
+        e.preventDefault()
+        let btn = e.target;
+        let form = btn.parentNode;
+        let id = form.dataset.id;
+        let name = form.name.value;
+        let address = form.address.value;
+        let text = form.text.value;
+        let dateNow = new Date();
+        let date = `${dateNow.getDate()}.${dateNow.getUTCMonth()}.${dateNow.getFullYear()}`;
+        let error = false;
     
-/*     clusterer.add(geoObjects);
-    map.geoObjects.add(clusterer);    */ 
+        let inputs = form.querySelectorAll('.modal-review__input');
+    
+        for (let input of inputs) {
+            if (input.value === '' && input.required) {
+                input.classList.add('required')
+                error = true
+            } else {
+                input.classList.remove('required')
+            }
+        }
+    
+        if (!error) {
+            
+            map.geoObjects.add(createPlacemark(id, lastCoords, lastAddress));
+            
+            let comment = comments.find(item => {
+                return item.id === id;
+            })
+            
+            if (!comment) {
+                comments.push({
+                    id,
+                    lastAddress,
+                    list: [{
+                        name,
+                        address,
+                        date,
+                        text  
+                    }]
+                })
+            } else {
+                comment.list.push({
+                    name,
+                    address,
+                    date,
+                    text  
+                })
+            }         
+            
+            let commentList = modal.querySelector('.modal__comments-list');
+    
+            commentList.innerHTML = generateComments(id);
+            form.reset()
+        }
+    })
 }
 
 ymaps.ready(init)
 
-function showModal( id, position, titleName) {
+function createPlacemark(id, coords, lastAddress) {
+    // Создаём макет содержимого.
+    let MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<i class="map-pin active fa fa-map-marker" aria-hidden="true"></i>'
+    )
+    const placemark = new ymaps.Placemark(coords, {}, {
+        // Необходимо указать данный тип макета.
+        iconImageHref: '',
+        // Размеры метки.
+        iconImageSize: [29, 50],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-14, -25],
+        iconLayout: 'default#imageWithContent',
+        iconContentLayout: MyIconContentLayout
+    })
+
+    placemark.properties.set('test-id', id);
+    placemark.properties.set('type', 'geoMarker');
+    placemark.properties.set('address', lastAddress);
+
+    return placemark;
+}
+
+function showModal(id, position, titleName) {
     modal.classList.add('active')
     let form = modal.querySelector('#add-review');
 
@@ -107,20 +145,23 @@ function showModal( id, position, titleName) {
 
     let positionLeft = position[0];
     let positionTop = position[1];
+
     let windowWidth = window.innerWidth;
     let windowHeight = window.innerHeight;
+
     let modalWidth = modal.offsetWidth;
     let modalHeight = modal.offsetHeight;
+
     let pinSize = {
         width: 29,
         height: 50
-    }    
+    }
 
     modal.style.left = `${positionLeft + pinSize.width / 2}px`
     modal.style.top = `${positionTop + pinSize.height / 2}px`
 
     if (positionLeft + modalWidth > windowWidth) {
-        modal.style.left = `${positionLeft - modalWidth}px`;        
+        modal.style.left = `${positionLeft - modalWidth}px`;
     }
 
     if (positionTop + modalHeight > windowHeight) {
@@ -133,7 +174,7 @@ function hideModal() {
     let commentList = modal.querySelector('.modal__comments-list');
 
     commentList.innerHTML = '';
-    title.innerHTML = '';    
+    title.innerHTML = '';
     modal.classList.remove('active');
 
     let inputs = modal.querySelectorAll('.modal-review__input');
@@ -141,69 +182,23 @@ function hideModal() {
     for (let input of inputs) {
         input.classList.remove('required')
         input.value = '';
-    }    
+    }
 }
 
 modalCloseBtn.addEventListener('click', hideModal)
 
-function addToComments(id, address) {
-    comments.push({
-        id,
-        address,
-        list: []
-    })    
-}
-
-function generateComments(id) {  
+function generateComments(id) {
     let comment = comments.find(item => {
-        return item.id === Number(id);
+        return item.id === id;
     })
     
-    let modalHtml = renderReview({ list: comment.list }) 
-    
-    if (!modalHtml) {
-        modalHtml = 'Отзывов ещё нет'
+    if (!comment) {
+        return 'Отзывов ещё нет'
     }
-    
+
+    let modalHtml = renderReview({
+        list: comment.list
+    })
+
     return modalHtml;
 }
-
-modalSaveReview.addEventListener('click', e => {
-    e.preventDefault()
-    let btn = e.target;
-    let form = btn.parentNode;
-    let id = form.dataset.id;
-    let name = form.name.value;
-    let address = form.address.value;
-    let text = form.text.value;
-    let dateNow = new Date();
-    let date = `${dateNow.getDate()}.${dateNow.getUTCMonth()}.${dateNow.getFullYear()}`;
-    let error = false;
-
-    let inputs = form.querySelectorAll('.modal-review__input');
-
-    for (let input of inputs) {
-        if (input.value === '' && input.required) {
-            input.classList.add('required')
-            error = true
-        } else {
-            input.classList.remove('required')
-        }
-    }
-    
-    if (!error) {
-        let comment = comments.find(item => {
-            return item.id === Number(id);
-        })
-    
-        comment.list.push({
-            name, address, date, text
-        })
-    
-        let commentList = modal.querySelector('.modal__comments-list');
-    
-        commentList.innerHTML = generateComments(id);
-    
-        form.reset()
-    }
-})
