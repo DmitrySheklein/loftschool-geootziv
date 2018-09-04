208dc9d10957e6cd7aad49b746783b92f51f6f0c
Dmitry <sheklein.dima@gmail.com>
2018-09-04 19:30:30 +0300

    Modal DnD in file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './styles/styles.scss';
 import renderReview from './../modal-review.hbs';
+import { modalDnD } from './modalDnD.js';
 
 let comments = [];
 let modal = document.getElementById('modal');
@@ -73,8 +74,13 @@ const init = () => {
         coordToAdress.then(res => {
             let obj = res.geoObjects.get(0);
 
-            lastAddress = obj.properties.get('name');
-      
+            lastAddress = obj.properties.get('name');   
+            
+/*             let placemark = createPlacemark(id, lastCoords, lastAddress);
+
+            map.geoObjects.add(placemark);
+            clusterer.add(placemark) */
+
             showModal(id, position, lastAddress)
             
         })
@@ -166,7 +172,7 @@ const init = () => {
 
 ymaps.ready(init)
 
-function createPlacemark(id, coords, lastAddress, content) {
+function createPlacemark(id, coords, lastAddress, content = '') {
     let MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
         '<i class="map-pin active fa fa-map-marker" aria-hidden="true"></i>'
     )
@@ -262,69 +268,4 @@ function generateComments(id) {
 
     return modalHtml;
 }
-
-const modalHeader = document.querySelector('.modal__header');
-let shift;
-let modalSize = {
-    width: null,
-    height: null,
-    offsetLeft: null,
-    offsetTop: null    
-}
-let windowWidth;
-let windowHeight;
-
-modalHeader.addEventListener('dragstart', evt => {
-    evt.dataTransfer.effectAllowed = 'move';
-    shift = {
-        left: `${evt.clientX - evt.target.offsetParent.offsetLeft}`,
-        top: `${evt.clientY - evt.target.offsetParent.offsetTop}`
-    }
-    modalSize.width = evt.target.offsetParent.clientWidth;
-    modalSize.height = evt.target.offsetParent.clientHeight;
-    
-    windowWidth = document.documentElement.clientWidth;
-    windowHeight = document.documentElement.clientHeight;
-     
-})
-
-modalHeader.addEventListener('drag', evt => {
-    modal.style.left = `${evt.clientX - shift.left}px`
-    modal.style.top = `${evt.clientY - shift.top}px`   
-    
-    modalSize.offsetLeft = evt.target.offsetParent.offsetLeft;
-    modalSize.offsetTop = evt.target.offsetParent.offsetTop;
-    
-    if (modalSize.offsetLeft + modalSize.width >= windowWidth) {
-        modal.style.left = `${windowWidth - modalSize.width}px`;
-    } else if (modalSize.offsetLeft <= 0) {
-        modal.style.left = '0px';        
-    }
-
-    if (modalSize.offsetTop + modalSize.height >= windowHeight) {
-        modal.style.top = `${windowHeight - modalSize.height}px`
-    } else if (modalSize.offsetTop <= 0) {
-        modal.style.top = '0px'
-    }
-})
-
-modalHeader.addEventListener('dragend', evt => {
-    evt.preventDefault(); 
-    modal.style.left = `${evt.clientX - shift.left}px`
-    modal.style.top = `${evt.clientY - shift.top}px`
-    
-    modalSize.offsetLeft = evt.target.offsetParent.offsetLeft;
-    modalSize.offsetTop = evt.target.offsetParent.offsetTop;
-
-    if (modalSize.offsetLeft + modalSize.width >= windowWidth) {
-        modal.style.left = `${windowWidth - modalSize.width}px`;
-    } else if (modalSize.offsetLeft <= 0) {
-        modal.style.left = '0px';
-    }
-
-    if (modalSize.offsetTop + modalSize.height >= windowHeight) {
-        modal.style.top = `${windowHeight - modalSize.height}px`
-    } else if (modalSize.offsetTop <= 0) {
-        modal.style.top = '0px'
-    }
-})
+modalDnD()
---
+++ src/modalDnD.js
@@ -0,0 +1,60 @@
+export function modalDnD() {
+    const modal = document.getElementById('modal');
+    const modalHeader = document.querySelector('.modal__header');
+    let shift;
+    let modalSize = {
+        width: null,
+        height: null,
+        offsetLeft: null,
+        offsetTop: null
+    }
+    let windowWidth;
+    let windowHeight;
+
+    modalHeader.addEventListener('dragstart', evt => {
+        let img = document.createElement('img');
+
+        img.src = '';
+        evt.dataTransfer.setDragImage(img, 0, 0);
+        evt.dataTransfer.effectAllowed = 'move';
+        shift = {
+            left: `${evt.clientX - evt.target.offsetParent.offsetLeft}`,
+            top: `${evt.clientY - evt.target.offsetParent.offsetTop}`
+        }
+        modalSize.width = evt.target.offsetParent.clientWidth;
+        modalSize.height = evt.target.offsetParent.clientHeight;
+
+        windowWidth = document.documentElement.clientWidth;
+        windowHeight = document.documentElement.clientHeight;
+
+    })
+
+    modalHeader.addEventListener('drag', evt => {
+        modalMove(evt);
+    })
+
+    modalHeader.addEventListener('dragend', evt => {
+        evt.preventDefault();
+        modalMove(evt);
+    })
+
+    function modalMove(evt) {
+        modal.style.left = `${evt.clientX - shift.left}px`
+        modal.style.top = `${evt.clientY - shift.top}px`
+
+        modalSize.offsetLeft = evt.target.offsetParent.offsetLeft;
+        modalSize.offsetTop = evt.target.offsetParent.offsetTop;
+
+        if (modalSize.offsetLeft + modalSize.width >= windowWidth) {
+            modal.style.left = `${windowWidth - modalSize.width}px`;
+        } else if (modalSize.offsetLeft <= 0) {
+            modal.style.left = '0px';
+        }
+
+        if (modalSize.offsetTop + modalSize.height >= windowHeight) {
+            modal.style.top = `${windowHeight - modalSize.height}px`
+        } else if (modalSize.offsetTop <= 0) {
+            modal.style.top = '0px'
+        }
+    }
+}
